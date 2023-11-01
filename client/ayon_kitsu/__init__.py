5ac0b14a20de493735944a2f11bc6a8ba43e2893
Jakub Trllo <jakub.trllo@gmail.com>
2023-11-01 12:16:48 +0100

    add '__version__' to '__init__.py'

--- client/ayon_kitsu/__init__.py
+++ client/ayon_kitsu/__init__.py
@@ -4,8 +4,11 @@ If addon class or settings definition won't be here their definition won't
 be found by OpenPype discovery.
 """
 
+from .version import __version__
 from .addon import KitsuAddon
 
+
 __all__ = (
+    "__version__",
     "KitsuAddon",
 )
