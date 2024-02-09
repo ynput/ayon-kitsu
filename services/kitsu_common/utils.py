93e9b6599b0c44be8adb320bd1a33cacb8b85110
Ember Light <jacob@emberlight.se>
2024-02-09 18:18:49 +0100

    Clean up code

--- services/kitsu_common/utils.py
+++ services/kitsu_common/utils.py
@@ -3,11 +3,8 @@ from typing import TYPE_CHECKING
 import gazu
 from nxtools import logging
 
-from .constants import kitsu_models, kitsu_statuses, kitsu_tasks
-
 if TYPE_CHECKING:
     import ayon_api
-    from ayon_api.entity_hub import ProjectEntity
 
 
 class KitsuServerError(Exception):
@@ -18,20 +15,19 @@ class KitsuSettingsError(Exception):
     pass
 
 
-def create_short_name(project_name: str) -> str:
-    project_name = project_name.lower()
-    code = project_name
+def create_short_name(name: str) -> str:
+    code = name.lower()
 
-    if "_" in project_name:
-        subwords = project_name.split("_")
+    if "_" in code:
+        subwords = code.split("_")
         code = "".join([subword[0] for subword in subwords])[:4]
-    elif len(project_name) > 4:
+    elif len(name) > 4:
         vowels = ["a", "e", "i", "o", "u"]
-        filtered_word = "".join([char for char in project_name if char not in vowels])
+        filtered_word = "".join([char for char in code if char not in vowels])
         code = filtered_word[:4]
 
-    # if there is a number at the end of the project_name, add it to the code
-    last_char = project_name[-1]
+    # if there is a number at the end of the code, add it to the code
+    last_char = code[-1]
     if last_char.isdigit():
         code += last_char
 
@@ -104,53 +100,3 @@ def get_kitsu_credentials(
     except AssertionError as e:
         logging.error(f"KitsuProcessor failed to initialize: {e}")
         raise KitsuSettingsError() from e
-
-
-def create_kitsu_entities_in_ay(
-    project_entity: "ProjectEntity",
-    kitsu_project: dict[str, str],
-):
-    """Ensure Ayon has all the Kitsu entitys, task types and statuses.
-
-    Args:
-        project_entity (ProjectEntity): The ProjectEntity for a given project.
-        kitsu_project (dict): The project owning the Tasks.
-    """
-
-    # Add Kitsu models as folder types to Project Entity
-    project_entity.set_folder_types(project_entity.get_folder_types() + kitsu_models)
-
-    ## Add Project task types to Project Entity
-    task_types = []
-
-    default_types = project_entity.get_task_types()
-    kitsu_types = gazu.task.all_task_types_for_project(kitsu_project)
-
-    for kitsu_type in kitsu_types:
-        new_type = {}
-        new_type["name"] = kitsu_type["name"]
-        # Fetch data from Ayon defaults
-        for default_type in default_types:
-            if new_type["name"] == default_type["name"]:
-                new_type["icon"] = default_type["icon"]
-                new_type["shortName"] = default_type["shortName"]
-
-        # Fetch icon from constants
-        constant_type = kitsu_tasks.get(new_type["name"].lower())
-        if constant_type:
-            new_type["icon"] = constant_type["icon"]
-
-        # Generate short name
-        if "shortName" not in new_type:
-            new_type["shortName"] = create_short_name(new_type["name"])
-        task_types.append(new_type)
-    project_entity.set_task_types(task_types)
-
-    # Add Kitsu task statuses to Project Entity
-    project_entity.set_statuses(gazu.task.all_task_statuses_for_project(kitsu_project))
-    # Add state and icon to the statuses
-    for status in project_entity.get_statuses():
-        kitsu_status = kitsu_statuses.get(status.short_name)
-        if kitsu_status:
-            status.set_icon(kitsu_status["icon"])
-            status.set_state(kitsu_status["state"])
