bcd1bcb96367405df235eab624d260426ce08e14
scott <scott@jammedia.ie>
2024-02-23 22:03:00 +0000

    tests for edits and concepts

---
+++ tests/tests/test_push_concept.py
@@ -0,0 +1,33 @@
+""" tests for endpoint 'api/addons/kitsu/{version}/push' 
+    with entities of kitsu type: Concept
+
+    $ poetry run pytest tests/test_push_concept.py
+"""
+
+from pprint import pprint
+
+import pytest
+
+from . import mock_data
+from .fixtures import (
+    PROJECT_NAME,
+    api,
+    kitsu_url,
+)
+
+
+def test_push_concepts(api, kitsu_url):
+    entities = mock_data.all_concepts_for_project
+
+    res = api.post(
+        f"{kitsu_url}/push",
+        project_name=PROJECT_NAME,
+        entities=entities,
+    )
+    assert res.status_code == 200
+    assert "folders" in res.data
+    assert list(res.data["folders"].keys()) == [
+        "concept-id-1",
+        "concept-id-2",
+        "concept-id-3",
+    ]
