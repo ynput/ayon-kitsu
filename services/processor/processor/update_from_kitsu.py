12d4cbe6f0760f91368db4355dd7db584aa4e7c5
scott <scott@jammedia.ie>
2024-03-06 13:57:24 +0000

    remove ayon_server_url as unnecessary

--- services/processor/processor/update_from_kitsu.py
+++ services/processor/processor/update_from_kitsu.py
@@ -19,9 +19,6 @@ def update_project(parent: "KitsuProcessor", data: dict[str, str]):
     # Get asset entity
     entity = gazu.project.get_project(data["project_id"])
 
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
-
     return ayon_api.post(
         f"{parent.entrypoint}/push",
         project_name=project_name,
@@ -37,7 +34,6 @@ def delete_project(parent: "KitsuProcessor", data: dict[str, str]):
 
     # Add ayon base url so we can use it in REST calls later on
     entity = {}
-    entity["ayon_server_url"] = ayon_api.get_base_url()
 
     return ayon_api.post(
         f"{parent.entrypoint}/push",
@@ -56,9 +52,6 @@ def create_or_update_asset(parent: "KitsuProcessor", data: dict[str, str]):
     entity = gazu.asset.get_asset(data["asset_id"])
     entity = utils.preprocess_asset(entity["project_id"], entity)
 
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
-
     return ayon_api.post(
         f"{parent.entrypoint}/push",
         project_name=project_name,
@@ -75,7 +68,6 @@ def delete_asset(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["asset_id"],
         "type": "Asset",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -91,9 +83,6 @@ def create_or_update_episode(parent: "KitsuProcessor", data: dict[str, str]):
         return  # do nothing as this kitsu and ayon project are not paired
     # Get episode entity
     entity = gazu.shot.get_episode(data["episode_id"])
-
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
 
     return ayon_api.post(
         f"{parent.entrypoint}/push",
@@ -111,7 +100,6 @@ def delete_episode(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["episode_id"],
         "type": "Episode",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -127,9 +115,6 @@ def create_or_update_sequence(parent: "KitsuProcessor", data: dict[str, str]):
         return  # do nothing as this kitsu and ayon project are not paired
 
     entity = gazu.shot.get_sequence(data["sequence_id"])
-
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
 
     return ayon_api.post(
         f"{parent.entrypoint}/push",
@@ -147,7 +132,6 @@ def delete_sequence(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["sequence_id"],
         "type": "Sequence",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -163,9 +147,6 @@ def create_or_update_shot(parent: "KitsuProcessor", data: dict[str, str]):
         return  # do nothing as this kitsu and ayon project are not paired
 
     entity = gazu.shot.get_shot(data["shot_id"])
-
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
 
     return ayon_api.post(
         f"{parent.entrypoint}/push",
@@ -183,7 +164,6 @@ def delete_shot(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["shot_id"],
         "type": "Shot",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -201,9 +181,6 @@ def create_or_update_task(parent: "KitsuProcessor", data: dict[str, str]):
     entity = gazu.task.get_task(data["task_id"])
     entity = utils.preprocess_task(entity["project_id"], entity)
 
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
-
     return ayon_api.post(
         f"{parent.entrypoint}/push",
         project_name=project_name,
@@ -220,7 +197,6 @@ def delete_task(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["task_id"],
         "type": "Task",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -238,9 +214,6 @@ def create_or_update_edit(parent: "KitsuProcessor", data: dict[str, str]):
     # Get edit entity
     entity = gazu.edit.get_edit(data["edit_id"])
 
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
-
     return ayon_api.post(
         f"{parent.entrypoint}/push",
         project_name=project_name,
@@ -257,7 +230,6 @@ def delete_edit(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["edit_id"],
         "type": "Edit",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -275,9 +247,6 @@ def create_or_update_concept(parent: "KitsuProcessor", data: dict[str, str]):
     # Get concept entity
     entity = gazu.concept.get_concept(data["concept_id"])
 
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
-
     return ayon_api.post(
         f"{parent.entrypoint}/push",
         project_name=project_name,
@@ -294,7 +263,6 @@ def delete_concept(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["concept_id"],
         "type": "Concept",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
@@ -306,9 +274,6 @@ def delete_concept(parent: "KitsuProcessor", data: dict[str, str]):
 def create_or_update_person(parent: "KitsuProcessor", data: dict[str, str]):
     logging.info(f"create_or_update_person: {data}")
     entity = gazu.person.get_person(data["person_id"])
-
-    # Add ayon base url so we can use it in REST calls later on
-    entity["ayon_server_url"] = ayon_api.get_base_url()
 
     return ayon_api.post(
         f"{parent.entrypoint}/push",
@@ -326,7 +291,6 @@ def delete_person(parent: "KitsuProcessor", data: dict[str, str]):
     entity = {
         "id": data["person_id"],
         "type": "person",
-        "ayon_server_url": ayon_api.get_base_url(),
     }
     return ayon_api.post(
         f"{parent.entrypoint}/remove",
