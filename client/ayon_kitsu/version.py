7293f422225dd6683bd9c02ba9cfbcf82f7f7aae
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-04-19 15:24:05 +0200

    bump version to '1.2.1-dev.1'

--- client/ayon_kitsu/version.py
+++ client/ayon_kitsu/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring kitsu addon version."""
-__version__ = "1.2.0"
+__version__ = "1.2.1-dev.1"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "kitsu"
 title = "Kitsu"
-version = "1.2.0"
+version = "1.2.1-dev.1"
 client_dir = "ayon_kitsu"
 
 services = {
