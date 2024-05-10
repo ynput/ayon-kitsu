e0da08fc3361364056f0be658bd8dc409e587c4e
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-10 11:49:29 +0200

    added requirement to ayon core

--- package.py
+++ package.py
@@ -6,3 +6,8 @@ client_dir = "ayon_kitsu"
 services = {
     "processor": {"image": f"ynput/ayon-kitsu-processor:{version}"},
 }
+
+ayon_required_addons = {
+    "core": ">=0.3.0",
+}
+ayon_compatible_addons = {}
