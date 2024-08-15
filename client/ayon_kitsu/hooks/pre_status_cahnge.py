16809f480ef78a3aa97b08fc50669dd0a3b22705
timsergeeff <38128238+timsergeeff@users.noreply.github.com>
2024-08-16 00:22:05 +0300

    Apply suggestions from code review
    Co-authored-by: Mustafa Jafar <mustafataherzaky@outlook.com>

--- client/ayon_kitsu/hooks/pre_status_cahnge.py
+++ client/ayon_kitsu/hooks/pre_status_cahnge.py
@@ -1,16 +1,16 @@
 from ayon_applications import PreLaunchHook
 import os
 
-class pre_status_change(PreLaunchHook):
+class PreStatusChange(PreLaunchHook):
     order = 1
     launch_types = ()
-    note_status_shortname="wip"
+    note_status_shortname = "wip"
 
     def execute(self):
         import gazu
-        kitsuId= self.data["task_entity"]['data']['kitsuId']
+        kitsuId = self.data["task_entity"]["data"]["kitsuId"]
         
-        self.log.debug(f'Changing Kitsu task status to WIP. Task id {kitsuId}')
+        self.log.debug(f"Changing Kitsu task status to WIP. Task id {kitsuId}")
 
         try:
             gazu.set_host(os.environ["KITSU_SERVER"])
@@ -18,8 +18,8 @@ class pre_status_change(PreLaunchHook):
             gazu.log_in(os.environ["KITSU_LOGIN"], os.environ["KITSU_PWD"])
 
             kitsu_status = gazu.task.get_task_status_by_short_name(
-                        self.note_status_shortname
-                    )
+                self.note_status_shortname
+            )
 
 
             task=gazu.task.get_task(kitsuId)
