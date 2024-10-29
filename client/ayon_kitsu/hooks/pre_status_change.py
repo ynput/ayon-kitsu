2df77f47035cdab214a12dd18bd3c6e112f48fbb
timsergeeff <tim.sergeeff2011@gmail.com>
2024-10-29 20:14:59 +0300

    fix tests

--- client/ayon_kitsu/hooks/pre_status_change.py
+++ client/ayon_kitsu/hooks/pre_status_change.py
@@ -8,9 +8,9 @@ class PreKitsuStatusChange(PostLaunchHook):
     """On launch of an application change task statuses on Kitsu.
 
     When launching an application this launch hook may change the
-    status on Kitsu with the relevant task id. 
+    status on Kitsu with the relevant task id.
 
-    If "pause other tasks" is enabled this will also change the status of 
+    If "pause other tasks" is enabled this will also change the status of
     all other tasks to the paused task status. This can be useful for
     X, Y, Z.
     """
@@ -30,35 +30,35 @@ class PreKitsuStatusChange(PostLaunchHook):
         "comment_template": "{comment}",
     }
     def execute(self):
-        if not "KITSU_LOGIN" in os.environ:
-            self.log.info(f"KITSU_LOGIN is not set. assuming rendeing in deadline. Skipping status.")
+        if "KITSU_LOGIN" not in os.environ:
+            self.log.info("KITSU_LOGIN is not set. assuming rendeing in deadline. Skipping status.")
             return
 
         data = self.launch_context.data
         project_settings = data["project_settings"]["kitsu"]["appstart"]
 
-        if project_settings["set_status_app_start_note"]:
+        if project_settings["set_status_app_start_note"] and self.is_kitsu_enabled_in_settings:
             set_status_app_start_note= True
-            self.log.info(f"Kitsu Status change is Enabled.")
+            self.log.info("Kitsu Status change is Enabled.")
         else:
-            self.log.info(f"Kitsu Status change is disabled.")
+            self.log.info("Kitsu Status change is disabled.")
             return
-        
+
         if not project_settings["app_start_status_shortname"]:
-            self.log.info(f"App starting status in not configured")
+            self.log.info("App starting status in not configured")
             return
         app_start_status_shortname= project_settings["app_start_status_shortname"]
 
         if project_settings["app_startstatus_change_conditions"]:
             status_conditions= project_settings["app_startstatus_change_conditions"]
         else:
-            self.log.info(f"Status change condotions are empty")
+            self.log.info("Status change condotions are empty")
 
         gazu.set_host(os.environ["KITSU_SERVER"])
         gazu.log_in(os.environ["KITSU_LOGIN"], os.environ["KITSU_PWD"])
 
         if not self.data["task_entity"]["data"].get("kitsuId"):
-            self.log.info(f"This task does not have a kitsu task id. Skipping kitsu task status change.")
+            self.log.info("This task does not have a kitsu task id. Skipping kitsu task status change.")
             return
         
         kitsuId = self.data["task_entity"]["data"]["kitsuId"]
@@ -76,20 +76,20 @@ class PreKitsuStatusChange(PostLaunchHook):
 
         if allow_status_change:
             if not gazu.task.get_task_status_by_short_name(self.app_start_status_shortname):
-                self.log.info(f"Failed to recieve kitsu status instance for shortname. Skipping Status change.")
+                self.log.info("Failed to recieve kitsu status instance for shortname. Skipping Status change.")
                 return
             kitsu_wip_status = gazu.task.get_task_status_by_short_name(self.app_start_status_shortname)
             
-            self.log.info(f"Current Kitsu task status is {task_current_status_shortname}. Task id {kitsuId}")
+            self.log.info("Current Kitsu task status is {task_current_status_shortname}. Task id {kitsuId}")
             
-            self.log.info(f"Changing Kitsu task status to {self.app_start_status_shortname}.")
+            self.log.info("Changing Kitsu task status to {self.app_start_status_shortname}.")
 
             gazu.task.add_comment(task["id"], kitsu_wip_status)
 
             if not project_settings["set_pause_status_to_other_tasks"]:
-                self.log.info(f"Pausing all other tasks with same status disabled.")
+                self.log.info("Pausing all other tasks with same status disabled.")
             else:
-                self.log.info(f"Pausing all other tasks with same status enabled.")
+                self.log.info("Pausing all other tasks with same status enabled.")
                 pause_status_shortname= project_settings["pause_status_shortname"]
                 if gazu.task.get_task_status_by_short_name(pause_status_shortname):
                     pause_status=gazu.task.get_task_status_by_short_name(pause_status_shortname)
@@ -100,7 +100,7 @@ class PreKitsuStatusChange(PostLaunchHook):
                         if same_status and not same_task:
                             gazu.task.add_comment(other_task["id"], pause_status,)
                 else:
-                     self.log.info(f"Failed to recieve kitsu pause status instance. Skipping pause Status change.")
+                     self.log.info("Failed to recieve kitsu pause status instance. Skipping pause Status change.")
         else:
-            self.log.info(f"Status not changed due to conditions: {status_conditions}")
+            self.log.info("Status not changed due to conditions: {status_conditions}")
         gazu.log_out() 
