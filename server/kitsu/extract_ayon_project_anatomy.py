c4bc5aad9cef8c93628a94e8105ab632553342dc
Martastain <martas@imm.cz>
2024-11-08 15:16:43 +0100

    doc: add note about backwards compatibility of extract anatomy function

--- server/kitsu/extract_ayon_project_anatomy.py
+++ server/kitsu/extract_ayon_project_anatomy.py
@@ -1,3 +1,16 @@
+"""Extract Anatomy object from ayon ProjectEntity.
+
+This file is for the backwards compatibility with Ayon server. 
+
+This funciton is a part of the server from version 1.5.1, so we
+can remove this file after a grace period.
+
+```
+from ayon_server.helpers.extract_anatomy import extract_project_anatomy
+```
+
+"""
+
 from typing import Any
 from ayon_server.entities import ProjectEntity
 from ayon_server.entities.models.submodels import LinkTypeModel
