e2af369222bc282c3528979bea91021e6b7ac34d
Roy Nieterau <roy_nieterau@hotmail.com>
2026-04-10 11:34:41 +0200

    Add as class attribute

--- client/ayon_kitsu/plugins/publish/integrate_kitsu_review.py
+++ client/ayon_kitsu/plugins/publish/integrate_kitsu_review.py
@@ -13,6 +13,8 @@ class IntegrateKitsuReview(KitsuPublishInstancePlugin):
     families = ["kitsu"]
     optional = True
 
+    match_version_number = True
+
     def process(self, instance):
         # Check comment has been created
         comment_id = instance.data.get("kitsuComment", {}).get("id")
