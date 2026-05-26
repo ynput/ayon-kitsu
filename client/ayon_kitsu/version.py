f77f6c381ffdab2400d1d569b4fb3354930005d2
qi.shiyuan.ani <qi.shiyuan.ani@gmail.com>
2026-05-26 12:19:36 +0200

    cleaning code

--- client/ayon_kitsu/version.py
+++ client/ayon_kitsu/version.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring kitsu addon version."""
-<<<<<<< HEAD
-__version__ = "1.2.7+dev-sync-status"
-=======
 __version__ = "1.2.7+dev"
->>>>>>> origin/develop
